746900b474eb9e014b498258e434545ef5de1aa9
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-04-23 17:46:17 +0900

    + Almost Complete.

--- client.go
+++ client.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/gorilla/websocket"
 )
 
+type statusType struct {
+	WholeResult int    `json:"whole_result"`
+	Result      int    `json:"result"`
+	Time        int64  `json:"time"`
+	Memory      int64  `json:"memory"`
+	CurrentCase int    `json:"current_case"`
+	WholeCase   int    `json:"whole_case"`
+	Description string `json:"description"`
+}
+
 func sendData(data []byte) {
+	var result resultType
+	var status statusType
 	conn, _, err := websocket.DefaultDialer.Dial("ws://localhost:7867/submit", nil)
 	if err != nil {
 		log.Fatalln(err)
@@ -20,6 +34,15 @@ func sendData(data []byte) {
 		if err != nil {
 			break
 		}
-		log.Println(string(bytes))
+		if err := json.Unmarshal(bytes, &status); err != nil {
+			log.Fatalln(err)
+		}
+		res := resultType(status.Result)
+		fmt.Printf("%d/%d %v %dms %dkb\n", status.CurrentCase, status.WholeCase, res, status.Time, status.Memory)
+		result.update(res)
+	}
+	fmt.Println(result)
+	if resultType(status.WholeResult) == resultCompileError {
+		fmt.Println(status.Description)
 	}
 }
